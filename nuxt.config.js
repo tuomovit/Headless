43eea38a5185a2ad576c1bee85116bbdc9b5a753
Tuomo <tuomo.vitikainen@gmail.com>
2022-03-31 13:53:09 +0300

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Headless WP',
+    title: 'Staattinen',
     htmlAttrs: {
       lang: 'en'
     },
