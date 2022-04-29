e59da52d35c7ea82269080862f50e903f6baec9b
Tuomo <tuomo.vitikainen@gmail.com>
2022-04-29 09:12:48 +0300

    meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,11 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'description' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      {
+        name: 'google-site-verification',
+        content: 'T6AzUzWefm1nfXnQ2fboOqgsGOAk6WqEpjBOoauBPbE'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
